5a618d70c93a063b5298f418e61f0170745e1c16
ardanimo <ardanimoxd@gmail.com>
2025-08-11 11:04:11 +0300

    postdto fields order change

--- Backend/BusinessLayer/Dtos/PostDto.cs
+++ Backend/BusinessLayer/Dtos/PostDto.cs
@@ -5,12 +5,12 @@ public class PostDto
   public Guid Id { get; set; } =  Guid.NewGuid();
   public Guid UserId { get; set; }
   public Guid? ParentId { get; set; } // null for main posts
+  public int Depth { get; set; }
   public string? Title { get; set; } //null for comments
   public string Content { get; set; }
-  public bool IsPinned { get; set; }
   public int VoteScore { get; set; }
   public int CommentCount { get; set; }
-  public int Depth { get; set; }
+  public bool IsPinned { get; set; }
   public bool IsEdited { get; set; }
   public bool IsDeleted { get; set; }
 }
