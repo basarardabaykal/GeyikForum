4f0c8c3d72dc98b285d9b970e17743b54c7e2ded
ardanimo <ardanimoxd@gmail.com>
2025-08-22 11:19:57 +0300

    error data result success bool fix

--- Backend/CoreLayer/Utilities/DataResults/Concretes/ErrorDataResult,.cs
+++ Backend/CoreLayer/Utilities/DataResults/Concretes/ErrorDataResult,.cs
@@ -5,11 +5,11 @@ namespace CoreLayer.Utilities.DataResults.Concretes;
 public class ErrorDataResult<T> : DataResult<T>
 {
   public ErrorDataResult(int statusCode, string errorMessage, T data)
-    : base(true, statusCode, errorMessage, data) { }
+    : base(false, statusCode, errorMessage, data) { }
   
   public ErrorDataResult(int statusCode, string errorMessage)
-    : base(true, statusCode, errorMessage, default(T)) { }
+    : base(false, statusCode, errorMessage, default(T)) { }
   
   public ErrorDataResult(int statusCode)
-    : base(true, statusCode, "Operation failed.", default(T)) { }
+    : base(false, statusCode, "Operation failed.", default(T)) { }
 }
